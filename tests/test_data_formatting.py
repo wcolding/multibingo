d73245676bcaceea3a64f62fd153c1aa018013ef
Will Colding <william.colding@gmail.com>
2024-01-05 22:20:10 -0500

    Add tests

---
+++ tests/test_data_formatting.py
@@ -0,0 +1,52 @@
+import unittest
+import os
+import io
+import json
+
+from ..src.Game import *
+
+class TestDataFormatting(unittest.TestCase):
+    game_dirs = []
+
+    @classmethod
+    def setUpClass(cls):
+        cls.game_dirs = os.listdir('games')
+
+    def test_a_json_named_correctly(self):
+        for game in self.game_dirs:
+            subdir = 'games/{folder}'.format(folder=game)
+            game_files = os.listdir(subdir)
+            self.assertIn('data.json', game_files)
+    
+    def test_b_json_format(self):
+        for game in self.game_dirs:
+            subdir = 'games/{folder}'.format(folder=game)
+            file_path = '{_subdir}/data.json'.format(_subdir = subdir)
+
+            file = io.open(file_path, 'r')
+            json_string = file.read()
+            file.close()
+
+            game_dict = json.loads(json_string)
+            self.assertIn('game', game_dict)
+            self.assertIn('checks', game_dict)
+
+    def test_c_checks_format(self):
+        for game in self.game_dirs:
+            subdir = 'games/{folder}'.format(folder=game)
+            file_path = '{_subdir}/data.json'.format(_subdir = subdir)
+
+            file = io.open(file_path, 'r')
+            json_string = file.read()
+            file.close()
+
+            game_dict = json.loads(json_string)
+            checks = game_dict['checks']
+            self.assertGreater(len(checks), 0)
+
+            for check in checks:
+                self.assertIn('name', check)
+                self.assertIn('obj_type', check)
+
+                self.assertEqual(check['name'], str(check['name']))
+                self.assertEqual(check['obj_type'], int(check['obj_type']))
