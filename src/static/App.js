a6848fdb902660c99e304d86236880078c37b657
Will Colding <william.colding@gmail.com>
2024-01-07 15:55:13 -0500

    Add button to expand game checks

---
+++ src/static/App.js
@@ -0,0 +1,12 @@
+function showGameChecks(game) {
+    let checks = document.getElementById(game + "_checks");
+    let button = document.getElementById(game + "_expandButton");
+
+    if (button.textContent == '+'){
+        checks.style.display = "block";
+        button.textContent = '-';
+    } else {
+        checks.style.display = "none";
+        button.textContent = '+';
+    }
+}
