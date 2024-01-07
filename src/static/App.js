bfae66ad5bbf28c442fa0b390457623d170aed11
Will Colding <william.colding@gmail.com>
2024-01-07 16:24:06 -0500

    String consistency

--- src/static/App.js
+++ src/static/App.js
@@ -2,11 +2,10 @@ function showGameChecks(game) {
     let checks = document.getElementById(game + "_checks");
     let button = document.getElementById(game + "_expandButton");
 
-    if (button.textContent == '+'){
+    if (button.textContent == "+"){
         checks.style.display = "block";
-        button.textContent = '-';
+        button.textContent = "-";
     } else {
         checks.style.display = "none";
-        button.textContent = '+';
-    }
+        button.textContent = "+";
 }
