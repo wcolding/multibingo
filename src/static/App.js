d874c23e7b236799022cca1bee698a78fbe52f93
Will Colding <william.colding@gmail.com>
2024-01-07 16:30:14 -0500

    Selecting game selects all checks

--- src/static/App.js
+++ src/static/App.js
@@ -8,4 +8,15 @@ function showGameChecks(game) {
     } else {
         checks.style.display = "none";
         button.textContent = "+";
+    }
+}
+
+function setAllGameChecks(game) {
+    let gameCheckBox = document.getElementById(game + "_allSelect");
+    let checksSection = document.getElementById(game + "_checks");
+
+    let checks = checksSection.getElementsByTagName("input");
+    for (let i = 0; i< checks.length; i++) {
+        checks[i].checked = gameCheckBox.checked;
+    }
 }
