bb3820ea9b8a11e0f3e6eb2cdbd22eb547edaba8
Will Colding <william.colding@gmail.com>
2024-01-08 14:45:31 -0500

    Tally up selected checks

--- src/static/App.js
+++ src/static/App.js
@@ -1,8 +1,12 @@
+let selectCounter = 0;
+let objectivesList = [];
+let randoCount = 25;
+
 function showGameChecks(game) {
     let checks = document.getElementById(game + "_checks");
     let button = document.getElementById(game + "_expandButton");
 
-    if (button.textContent == "+"){
+    if (button.textContent == "+") {
         checks.style.display = "block";
         button.textContent = "-";
     } else {
@@ -16,7 +20,51 @@ function setAllGameChecks(game) {
     let checksSection = document.getElementById(game + "_checks");
 
     let checks = checksSection.getElementsByTagName("input");
-    for (let i = 0; i< checks.length; i++) {
+    for (let i = 0; i < checks.length; i++) {
         checks[i].checked = gameCheckBox.checked;
+    }
+
+    tallyChecks();
+}
+
+function tallyChecks() {
+    selectCounter = 0;
+    objectivesList = [];
+    let checkElements = document.getElementsByClassName("checkElement");
+    for (let i = 0; i < checkElements.length; i++) {
+        let check = checkElements[i].getElementsByTagName("input")[0];
+        if (check.checked) {
+            let checkGame = check.parentElement.getElementsByClassName("checkGame")[0].innerHTML;
+            let checkObjType = check.parentElement.getElementsByClassName("checkObjType")[0].innerHTML;
+            let newObjType = `${checkGame}:${checkObjType}`;
+            if (!objectivesList.includes(newObjType)) {
+                objectivesList.push(newObjType);
+            }
+            selectCounter++;
+        }
+    }
+
+    updateCounterDisplay();
+}
+
+function updateCounterDisplay() {
+    let checksCounter = document.getElementsByClassName("checksCounter")[0];
+    let objsCounter = document.getElementsByClassName("objsCounter")[0];
+    checksCounter.innerHTML = `Checks: ${selectCounter}`;
+    objsCounter.innerHTML = `Objectives: ${objectivesList.length}`;
+    setColor(checksCounter, selectCounter, randoCount);
+    let enoughObjectives = setColor(objsCounter, objectivesList.length, randoCount);
+    if (enoughObjectives) {
+        // Todo: Enable generation
+    }
+}
+
+function setColor(object, counter, required) {
+    if (counter < required) {
+        object.style.color = "red";
+        return false;
+    } else {
+        object.style.color = "green";
+        return true;
     }
 }
