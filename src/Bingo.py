96963d58b4e8b35982f25111ae9ffe2feef1c2fd
Will Colding <william.colding@gmail.com>
2024-01-09 17:17:21 -0500

    Use game class, check shared objects

--- src/Bingo.py
+++ src/Bingo.py
@@ -22,6 +22,41 @@ for game in games:
     for check in game.checks_list:
         print(f'{game.name}: {check.name}')
 
+def GetNextCheck(game_json: dict) -> BingoCheck | None:
+    next_json = game_json['checks'].pop()
+    selected_game = None
+
+    for game in games:
+        if game.name == game_json['game']:
+            selected_game = game
+
+    if selected_game != None:
+        for check in selected_game.checks_list:
+            if check.name == next_json['name']:
+                return check
+            
+def CheckObjectiveUsed(game_name: str, check: BingoCheck, collection: list) -> bool:
+    obj_string = '{name}:{id}'.format(name = game_name, id = str(check.obj_type))
+    if obj_string in collection:
+        return True
+    else:
+        collection.append(obj_string)
+        print(collection)
+        return False
+
+def CheckSharedObjectiveUsed(game_name: str, check: BingoCheck, collection: list) -> bool:
+    if CheckObjectiveUsed(game_name, check, collection):
+        return True
+    
+    for shared_game in check.shared:
+        print(f'Checking "{check.name}" in shared game {shared_game}...')
+        if (CheckObjectiveUsed(shared_game, check, collection)):
+            return True
+
+        
+    return False
+
+
 def GenerateBoard(data: list, target: int, include_game_name = False, balancing = True) -> str:
     generating = True
     board = []
@@ -35,15 +70,14 @@ def GenerateBoard(data: list, target: int, include_game_name = False, balancing 
             for game in data:
                 if len(board) < target:
                     if game['checks']:
-                        next_check = game['checks'].pop()
-                        obj_string = '{game_name}:{obj_id}'.format(game_name = game['game'], obj_id = str(next_check['obj_type']))
-                        if obj_string not in used_objectives:
-                            used_objectives.append(obj_string)
-                            new_check = {}
-                            new_check['name'] = next_check['name']
-                            if include_game_name:
-                                new_check['name'] = '[{game_name}] {check_name}'.format(game_name = game['game'], check_name = new_check['name']) 
-                            board.append(new_check)
+                        next_check = GetNextCheck(game)
+                        if next_check != None:
+                            if not CheckSharedObjectiveUsed(game['game'], next_check, used_objectives):
+                                new_check = {}
+                                new_check['name'] = next_check.name
+                                if include_game_name:
+                                    new_check['name'] = '[{game_name}] {check_name}'.format(game_name = game['game'], check_name = new_check['name']) 
+                                board.append(new_check)
         else:
             pass # todo: make one new big list from which to generate
 
