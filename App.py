c2f3a8fd4308d1700527b7158bd769ab5b74cd1b
Will Colding <william.colding@gmail.com>
2024-01-18 16:38:11 -0500

    Clicking banner now resets

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ from src.Bingo import games, presets, GenerateBoard
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
     return render_template('App.html', games = games, presets = presets, generated = '', last_setting = '')
 
